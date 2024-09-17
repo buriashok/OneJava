5e48a36e03c864c5033a3de78080f4c62e63a5eb
Ashok kumar <buri.ashokkumar@gmail.com>
2024-09-17 23:58:34 +0530

    Create priPatta.java
    this program print the pattern of 1 for first row 22 for second  row so on

---
+++ priPatta.java
@@ -0,0 +1,14 @@
+public class priPatta {
+    public static void main(String[] args){
+        int num2 = 1;
+        for (int i = 0; i < 5; i++) {
+            int apple = num2;
+            while (apple > 0) {
+                System.out.print(num2);
+                apple--;
+            }
+            num2++;
+            System.out.println(" ");
+        }
+    }
+}
