379f03fbbec5b4bca29ecdab786e0e0e6b626e76
Ashok kumar <buri.ashokkumar@gmail.com>
2024-09-18 19:16:15 +0530

    print the heap triangle
    this program prints heap triangle

---
+++ printPattE.java
@@ -0,0 +1,14 @@
+public class printPattE {
+    public static void main(String[] args){
+        int row=5;
+        for(int i =0;i<row;i++){
+            for(int j=row-i;j>=0;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
