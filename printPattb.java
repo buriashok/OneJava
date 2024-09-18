7ebe060484bff9701e546c7097c1ddd549122f54
Ashok kumar <buri.ashokkumar@gmail.com>
2024-09-18 18:00:54 +0530

    Print Star Pattern
    printing the upper triangle pattern with stars.

---
+++ printPattb.java
@@ -0,0 +1,13 @@
+public class printPattb {
+    public static void main(String[] args){
+        int i,j=0;
+        for( i=1;i < 6;i++){
+            j=i;
+            while(j>0){
+                System.out.print("*");
+                j--;
+            }
+            System.out.println(" ");
+        }
+    }
+}
